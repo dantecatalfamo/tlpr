1c09e7aa2a48ba388b989398bebbeb3948535596
dantecatalfamo <dante.catalfamo@gmail.com>
2021-10-27 20:42:24 -0400

    Fix rubocop

--- easyprint.rb
+++ easyprint.rb
@@ -6,55 +6,59 @@ require 'optparse'
 require 'yaml'
 
 CUT_CMD = "\n\n\n\n\u001DV\u0001"
-CONF_FILE = File.expand_path("~/.config/easyprint/easyprint.yaml")
+CONF_FILE = File.expand_path('~/.config/easyprint/easyprint.yaml')
 
 Options = Struct.new(:cut, :ip, :file)
 options = Options.new
 
-if File.exists?(CONF_FILE)
+if File.exist?(CONF_FILE)
   conf = YAML.load_file(CONF_FILE)
-  options.ip = conf["IP"]
-  options.cut = conf["CUT"]
+  options.ip = conf['IP']
+  options.cut = conf['CUT']
 end
 
 OptionParser.new do |opts|
-  opts.banner = <<~END
-  Usage: #{$PROGRAM_NAME} [options] [args...]
+  opts.banner = <<~BANNER
+    Usage: #{$PROGRAM_NAME} [options] [args...]
 
-  Print either args or stdin, or file through thermal printer.
-  It has a config file is #{CONF_FILE} in the following format:
+    Print either args or stdin, or file through thermal printer.
+    The configuration file is located at #{CONF_FILE}
+  BANNER
 
-  ---
-  IP: 192.168.0.5
-  CUT: true
-
-  END
-
-  opts.on("-c", "--[no-]cut", "Cut after printing") do |cut|
+  opts.on('-c', '--[no-]cut', 'Cut after printing') do |cut|
     options.cut = cut
   end
 
-  opts.on("-iIP", "--ip=IP", "Printer IP address") do |ip|
+  opts.on('-iIP', '--ip=IP', 'Printer IP address') do |ip|
     options.ip = ip
   end
 
-  opts.on("-fFILE", "--file=FILE", "Print the contents of FILE") do |file|
+  opts.on('-fFILE', '--file=FILE', 'Print the contents of FILE') do |file|
     options.file = file
   end
+
+  opts.on('--example-config', 'Print an example configuration to stdout') do
+    puts <<~CONFIG
+      ---
+      IP: 192.168.0.5
+      CUT: true
+    CONFIG
+    exit
+  end
 end.parse!
+
+abort 'No IP configured.' unless options.ip
 
 socket = TCPSocket.new(options.ip, 9100)
 
 if options.file
   socket.write(File.read(options.file))
 elsif ARGV.empty?
-  STDIN.each_line do |line|
+  $stdin.each_line do |line|
     socket.write(line)
   end
 else
-  socket.write(ARGV.join(' ') + "\n")
+  socket.write("#{ARGV.join(' ')}\n")
 end
 
-if options.cut
-  socket.write(CUT_CMD)
-end
+socket.write(CUT_CMD) if options.cut
